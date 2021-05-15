ba6415ac372015c1dc88d822886c555f6d351daf
Ross MacArthur <ross@macarthur.io>
2021-05-15 14:21:58 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::env;
+use std::error::Error;
+use std::iter;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Alfred passes in a single argument for the user query.
+    let query = env::args().nth(1);
+
+    // Create an item to show in the Alfred drop down.
+    let item = powerpack::Item::new("Hello world!")
+        .subtitle(format!("Your query was '{:?}'", query))
+        .icon(powerpack::Icon::from_file_type("public.script"));
+
+    // Output the item to Alfred!
+    powerpack::output(iter::once(item))?;
+
+    Ok(())
+}
