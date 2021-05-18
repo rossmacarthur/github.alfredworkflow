24bb31d63384809285b66d24fe71c04e5f280ec7
Ross MacArthur <ross@macarthur.io>
2021-05-18 13:22:53 +0200

    Add useful shortcuts, prefixed with `/`

--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,69 @@ mod cache;
 mod github;
 
 use std::env;
+use std::vec;
 
 use anyhow::Result;
 use itertools::Itertools;
-use powerpack::{Icon, Item};
+use powerpack::{Icon, Item, String as Str};
 
-fn to_item(repo: github::Repo) -> Item<'static> {
+const SHORTCUTS: &[(&str, &str)] = &[
+    ("/feed", "/"),
+    ("/issues", "/issues"),
+    ("/notifications", "/notifications"),
+    ("/pulls", "/pulls"),
+    ("/settings", "/settings"),
+];
+
+fn shortcuts() -> vec::IntoIter<(Str<'static>, Str<'static>)> {
+    let mut shortcuts: Vec<_> = SHORTCUTS
+        .iter()
+        .map(|&(a, b)| (Str::from(a), Str::from(b)))
+        .sorted_by(|a, b| a.0.cmp(&b.0))
+        .collect();
+    if let Some(user) = powerpack::env::var("GITHUB_USER") {
+        shortcuts.insert(0, ("/profile".into(), format!("/{}", user).into()));
+    }
+    shortcuts.into_iter()
+}
+
+fn repo_to_item(repo: github::Repo) -> Item<'static> {
     Item::new(format!("{}/{}", repo.owner.login, repo.name))
         .arg(repo.url())
+        .icon(Icon::new("icon.png"))
+}
+
+fn shortcut_to_item<'a>(shortcut: (Str<'a>, Str<'a>)) -> Item<'a> {
+    Item::new(shortcut.0)
+        .arg(format!("https://github.com{}", shortcut.1))
         .icon(Icon::new("icon.png"))
 }
 
 fn run(query: Option<&str>) -> Result<()> {
     let repos = cache::repos()?;
     match query {
-        Some("") | None => powerpack::output(repos.into_iter().map(to_item)),
+        Some("") | None => powerpack::output(repos.into_iter().map(repo_to_item)),
+        Some(query) if query.starts_with('/') => powerpack::output(
+            shortcuts()
+                .filter(|(s, _)| s.starts_with(query))
+                .map(shortcut_to_item),
+        ),
         Some(query) => powerpack::output(
             repos
                 .into_iter()
                 .filter(|repo| repo.owner.login.starts_with(query) || repo.name.starts_with(query))
                 .sorted_by(|a, b| (&a.name, &a.owner.login).cmp(&(&b.name, &b.owner.login)))
-                .map(to_item),
+                .map(repo_to_item)
+                .chain(
+                    shortcuts()
+                        .filter(|(s, _)| s[1..].starts_with(query))
+                        .map(shortcut_to_item),
+                ),
         ),
     }?;
     Ok(())
 }
 
 fn main() -> Result<()> {
-    run(env::args().nth(1).as_deref().map(|s| s.trim()))
+    run(env::args().nth(1).as_deref().map(str::trim))
 }
